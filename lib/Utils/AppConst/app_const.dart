d1c68f97f6558fc8ebb968f567324ff983d12615
sesakilhossen302 <cse.sakilhossem@gmail.com>
2026-09-07 18:37:28 +0600

    feat(core): add AppConst application constants

---
+++ lib/Utils/AppConst/app_const.dart
@@ -0,0 +1,5 @@
+class AppConst {
+  static const String appName = "কৃষি বাজার";
+  static const String appTagline = "কৃষকের ফসল, সরাসরি পাইকারি বাজারে";
+  static const String currencySymbol = "৳";
+}
